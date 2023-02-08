251bd611d2724da476e08a985bb82674890eb5d8
mauricioc08 <mauricio-cassiano@hotmail.com>
2023-02-08 12:16:05 -0300

    create a new project in reactjs to practice api request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './style.css'
 
 function App() {
+  const [nutri, setNutri] = useState([])
+
+  useEffect(() => {
+    function loadApi() {
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+      fetch(url)
+        .then(r => r.json())
+        .then(json => {
+          setNutri(json)
+        })
+    }
+    loadApi()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <strong>React Nutri</strong>
       </header>
+
+      {nutri.map(item => {
+        return (
+          <article className="post" key={item.id}>
+            <strong className="titulo">{item.titulo}</strong>
+            <img className="capa" src={item.capa} alt={item.titulo} />
+            <p className="subtitulo">{item.subtitulo}</p>
+            <a className="botao" href="#">
+              Acessar
+            </a>
+          </article>
+        )
+      })}
+
+      <footer>
+        <p>Todos os Direitos Reservados ©</p>
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
